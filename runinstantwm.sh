add875c09714e861f988db38201505a6ae5924ed
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-12-03 11:53:25 +0530

    run script updated

--- runinstantwm.sh
+++ runinstantwm.sh
@@ -1,10 +1,27 @@
+# Mail
+thunderbird &
+
+# Notification daemon that uses libnotify
 pkill /usr/lib/xfce4/notifyd/xfce4-notifyd
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
+
+# Dual screen setup script
 ~/.screenlayout/dual.sh &
-thunderbird --headless &
+
+# Statusbar script
 /home/angad/Repos/instantWM/statusbar.sh &
+
+# Wallpaper shifting script
 ~/Photos/nitrogen-wallpaper-switch.sh &
+
+# Keyboard typing rate
 xset r rate 200 25
+
+# Mounting my raspberry pi music directory
 sshfs ubuntu@4pi:/mnt/hdd/Music/liked ~/PiMount &
+
+# Network manager applet
 nm-applet &
+
+# Starting instantwm
 exec instantwm
--- statusbar.sh
+++ statusbar.sh
@@ -3,8 +3,9 @@ while true; do
   	mem=`free -h | awk '/^Mem/ { print $3"/"$2 }' | sed s/i//g`
   	cpu=`cat /proc/loadavg | awk '{print $1}'`
   	datevar=`date '+%b %d (%a) %I:%M%p::%Ss'`
-  	bat=`acpi | cut -d' ' -f4-5`
+  	bat=`acpi | cut -d' ' -f3-`
 
   	xsetroot -name "  $cpu% <<<   $mem <<<   $datevar <<<  $bat"
 
+	sleep 0.001
 done
